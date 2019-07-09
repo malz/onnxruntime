2f83271f0380c6dce5655f6c549416902b5e2ec9
Changming Sun <chasun@microsoft.com>
2019-07-09 15:44:19 -0700

    sample for imagenet and batch prediction

---
+++ samples/c_cxx/imagenet/run_release.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+LD_LIBRARY_PATH=/home/chasun/src/onnxruntime/cmake/cmake-build-relwithdebinfo ../cmake-build-relwithdebinfo/imagenet/image_classifier /home/chasun/src/imagnet_validation_data "/home/chasun/src/onnxruntime/scripts/opset_10/inception_v4/model.onnx" /home/chasun/src/onnxruntime/samples/c_cxx/imagenet/imagenet_lsvrc_2015_synsets.txt /home/chasun/src/onnxruntime/samples/c_cxx/imagenet/imagenet_2012_validation_synset_labels.txt
